576ef4bc37a68ef2abb3b0929fc5cd17aff6a50f
vamshi krishna prime <vamshi.krishna.prime@gmail.com>
2019-05-31 23:21:33 +0530

    Update rude_words.py

--- rude_words.py
+++ rude_words.py
@@ -8,15 +8,26 @@ rude_words_list = ["idiot",
                    "jerk",
                    "darn"]
 
-with open("rude_passage.py") as my_file:
-    contents = my_file.read()
-    words = contents.split(" ")
-    rude_words_count = 0
+def check_line(line, count):
+    rude_words_line_count = 0
+    words = line.split(" ")
     for word in words:
         if word in rude_words_list:
-            rude_words_count += 1
-            print(f"Rude word found: {rude_words_count}. {word}\n")
+            rude_words_line_count += 1
+            print(f"Rude word found: {count + 1}. {word}\n")
+    return rude_words_line_count
 
-if rude_words_count == 0:
-    print("Congratulations there are no rude words found")
-    print("Atleast none I know of")
+
+def check_file(filename):
+    with open(filename) as my_file:
+        rude_words_count = 0
+        for line in my_file:
+            rude_words_count += check_line(line, rude_words_count)
+    print(f"Total Rude Words Found: {rude_words_count}")
+    if rude_words_count == 0:
+        print("Congratulations there are no rude words found")
+        print("Atleast none I know of")
+
+
+if __name__ == "__main__":
+    check_file("rude_passage.py")
